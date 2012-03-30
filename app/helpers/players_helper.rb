a171e9c48474bb03d566a364c8b2d4072e2b7d27
Nick DeSteffen <nick.desteffen@gmail.com>
2012-03-29 20:26:25 -0500

    Tweaking html on players

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -3,5 +3,20 @@ module PlayersHelper
   def us_states_options
     States::STATES
   end
+
+  def location(player)
+    address = ""
+    if player.city.present? && player.state.present?
+      address = player.city + ", " + player.state
+    end
+    if player.zip_code.present?
+      address = address + " " + player.zip_code
+    end
+    if address.present?
+      return address
+    else
+      return "Unknown"
+    end
+  end
   
 end
