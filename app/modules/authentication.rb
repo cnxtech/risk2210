49f220b471ef52499f626a73ac5112bebf24a05c
Nick DeSteffen <nick.desteffen@gmail.com>
2012-07-07 13:10:08 -0500

    Cleaning up login method

--- app/modules/authentication.rb
+++ app/modules/authentication.rb
@@ -41,10 +41,10 @@ module Authentication
   end
 
   def login(player, options={})
-    path = options.fetch(:redirect_to) { new_game_path }
-    notice = options.fetch(:notice) { "Welcome Back!" }
+    path = options.fetch(:redirect_to, new_game_path)
+    notice = options.fetch(:notice, "Welcome Back!")
+    remember_me = options.fetch(:remember_me, "0").to_boolean
     
-    remember_me = options.fetch(:remember_me) { "0" }.to_boolean
     cookies.signed[:remember_me_token] = {value: player.remember_me_token, expires: 1.month.from_now} if remember_me
 
     session[:player_id] = player.id
