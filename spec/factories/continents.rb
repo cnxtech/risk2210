73214b5b71a28701a78bf09648d5821ea5ccd2d1
Nick DeSteffen <nick.desteffen@gmail.com>
2012-08-09 19:20:09 -0500

    Continent specs

--- spec/factories/continents.rb
+++ spec/factories/continents.rb
@@ -1,6 +1,27 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :continent do
+    name "North America"
+    type "Land"
+    bonus 5
+    color "#eae74a"
+    created_at { Time.now }
+    updated_at { Time.now }
   end
+
+  factory :land_continent, parent: :continent do
+    type "Land"
+  end
+
+  factory :water_continent, parent: :continent do
+    type "Water"
+  end
+
+  factory :lunar_continent, parent: :continent do
+    type "Lunar"
+  end
+
+  factory :lava_continent, parent: :continent do
+    type "Lava"
+  end
+
 end
--- spec/models/continent_spec.rb
+++ spec/models/continent_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Continent do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "ordered" do
+    it "should order continents by type first and then by name" do
+      aland = FactoryGirl.create(:land_continent, name: "a")
+      awater = FactoryGirl.create(:water_continent, name: "a")
+      alava = FactoryGirl.create(:lava_continent, name: "a")
+      alunar = FactoryGirl.create(:lunar_continent, name: "a")
+      tlunar = FactoryGirl.create(:lunar_continent, name: "t")
+      twater = FactoryGirl.create(:water_continent, name: "t")
+      tland = FactoryGirl.create(:land_continent, name: "t")
+      tlava = FactoryGirl.create(:lava_continent, name: "t")
+
+      continents = Continent.ordered
+
+      continents.should == [aland, tland, alava, tlava, alunar, tlunar, awater, twater]
+    end
+  end
+
 end
