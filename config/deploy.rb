ac018fbb576ae1f89560fbf03384ae4d7f4f22d4
Nick DeSteffen <nick.desteffen@gmail.com>
2012-09-22 15:47:40 -0500

    Capistrano task to dump database and restore locallay

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, "risk2210"
 set :scm, :git
 set :repository,  "git@github.com:nick-desteffen/risk2210.git"
 set :branch, "master"
-set :user, "commander"
+set :user, "ubuntu"
 set :deploy_to, "/opt/apps/risk2210"
 set :use_sudo, false
 
@@ -20,6 +20,16 @@ namespace :deploy do
   task :stop do ; end
   task :restart, roles: :app, except: { no_release: true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+namespace :db do
+  task :fetch do
+    temp_path = "./tmp"
+    run "cd /tmp && mongodump -db risk2210_production -o /tmp && tar --remove-files -czf risk2210_production.tar.gz risk2210_production"
+    download "/tmp/risk2210_production.tar.gz", "#{temp_path}/risk2210_production.tar.gz"
+    system "cd #{temp_path} && tar -xzvf risk2210_production.tar.gz"
+    system "mongorestore -d risk2210_development #{temp_path}/risk2210_production"
   end
 end
 
