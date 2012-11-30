9444d30ba2249d05a705b7c23e612274c7ee630b
Nick DeSteffen <nick.desteffen@gmail.com>
2012-11-29 20:41:18 -0600

    Formating facebook fixtures

--- spec/support/fixtures.rb
+++ spec/support/fixtures.rb
@@ -45,11 +45,177 @@ module Fixtures
     end
 
     def self.dude
-      {"provider"=>"facebook", "uid"=>"564037227", "info"=>{"nickname"=>"LALBT", "email"=>"knight829@gmail.com", "name"=>"Luis Alexander Lemus", "first_name"=>"Luis", "last_name"=>"Lemus", "image"=>"http://graph.facebook.com/564037227/picture?type=square", "description"=>"I study music and everything that has to do with music all the aspects of it from musician to all the technical stuff i play Violin , guitar, bass, drums and the piano", "urls"=>{"Facebook"=>"http://www.facebook.com/LALBT"}}, "credentials"=>{"token"=>"AAADMjLRob70BAMzcBY3eCZCxgnm3tR1sJGCw8ZBst57qdaFaBCe01F7ZAJkLlq6kY6gjdcSK1BBEgtKbUY3KqEByb8lFbTQB1JORr20QQZDZD", "expires_at"=>1347832925, "expires"=>true}, "extra"=>{"raw_info"=>{"id"=>"564037227", "name"=>"Luis Alexander Lemus", "first_name"=>"Luis", "middle_name"=>"Alexander", "last_name"=>"Lemus", "link"=>"http://www.facebook.com/LALBT", "username"=>"LALBT", "hometown"=>{"id"=>"109216849096476", "name"=>"Centreville, Virginia"}, "bio"=>"I study music and everything that has to do with music all the aspects of it from musician to all the technical stuff i play Violin , guitar, bass, drums and the piano", "quotes"=>"\"Why not 4:21\"\r\nme talkin to dakota and stevie\r\n\r\n\"Fo Free\"\r\nThat guy from that video\r\n\r\n\"Oh shit dry grass someone go put it out\r\n\r\n\"Jeepers Creepers\"\r\nDakota\r\n\r\n\"My Jeep its a freakin Transformer\"\r\nDakota", "work"=>[{"employer"=>{"id"=>"156223891096511", "name"=>"Fat Crow Antara"}, "position"=>{"id"=>"128674630510351", "name"=>"Audio Engineer"}, "start_date"=>"2011-08"}, {"employer"=>{"id"=>"223742647668475", "name"=>"Atomic Thoughts"}, "position"=>{"id"=>"128674630510351", "name"=>"Audio Engineer"}, "start_date"=>"2011-07"}, {"employer"=>{"id"=>"245321145541707", "name"=>"Cinbersol"}, "position"=>{"id"=>"128674630510351", "name"=>"Audio Engineer"}, "start_date"=>"2011-03"}, {"employer"=>{"id"=>"116860741677367", "name"=>"Freelance Violinist"}, "position"=>{"id"=>"145617765464974", "name"=>"Violinist"}, "start_date"=>"2009-04"}, {"employer"=>{"id"=>"111078042263414", "name"=>"Musician"}, "projects"=>[{"id"=>"109871969042969", "name"=>"Musician for hire", "start_date"=>"2010-09"}]}], "education"=>[{"school"=>{"id"=>"111845662166573", "name"=>"Westfield High School"}, "year"=>{"id"=>"141778012509913", "name"=>"2008"}, "type"=>"High School"}, {"school"=>{"id"=>"157433874304051", "name"=>"G. Martell"}, "concentration"=>[{"id"=>"136073143125360", "name"=>"Music Production & Engineering"}], "type"=>"College"}], "gender"=>"male", "email"=>"knight829@gmail.com", "timezone"=>-5, "locale"=>"en_US", "verified"=>true, "updated_time"=>"2012-05-13T03:41:11+0000"}}}
+      {
+        "provider" => "facebook",
+        "uid" => "564037227",
+        "info" => {
+          "nickname" => "LALBT",
+          "email" => "knight829@gmail.com",
+          "name" => "Luis Alexander Lemus",
+          "first_name" => "Luis",
+          "last_name" => "Lemus",
+          "image" => "http://graph.facebook.com/564037227/picture?type=square",
+          "description" => "I study music and everything that has to do with music all the aspects of it from musician to all the technical stuff i play Violin , guitar, bass, drums and the piano",
+          "urls" => {
+            "Facebook" => "http://www.facebook.com/LALBT"
+          }
+        },
+        "credentials" => {
+          "token" => "AAADMjLRob70BAMzcBY3eCZCxgnm3tR1sJGCw8ZBst57qdaFaBCe01F7ZAJkLlq6kY6gjdcSK1BBEgtKbUY3KqEByb8lFbTQB1JORr20QQZDZD",
+          "expires_at" => 1347832925,
+          "expires" => true
+        },
+        "extra" => {
+          "raw_info" => {
+            "id" => "564037227",
+            "name" => "Luis Alexander Lemus",
+            "first_name" => "Luis",
+            "middle_name" => "Alexander",
+            "last_name" => "Lemus",
+            "link" => "http://www.facebook.com/LALBT",
+            "username" => "LALBT",
+            "hometown" => {
+              "id" => "109216849096476",
+              "name" => "Centreville, Virginia"
+            },
+            "bio" => "I study music and everything that has to do with music all the aspects of it from musician to all the technical stuff i play Violin , guitar, bass, drums and the piano", "quotes"=>"\"Why not 4:21\"\r\nme talkin to dakota and stevie\r\n\r\n\"Fo Free\"\r\nThat guy from that video\r\n\r\n\"Oh shit dry grass someone go put it out\r\n\r\n\"Jeepers Creepers\"\r\nDakota\r\n\r\n\"My Jeep its a freakin Transformer\"\r\nDakota",
+            "work" => [
+              {
+                "employer" => {
+                  "id" => "156223891096511",
+                  "name" => "Fat Crow Antara"
+                },
+                "position" => {
+                  "id" => "128674630510351",
+                  "name" => "Audio Engineer"
+                },
+                "start_date" => "2011-08"
+              },
+              {
+                "employer" => {
+                  "id" => "223742647668475",
+                  "name" => "Atomic Thoughts"
+                },
+                "position" => {
+                  "id" => "128674630510351",
+                  "name" => "Audio Engineer"
+                },
+                "start_date" => "2011-07"
+              },
+              {
+                "employer" => {
+                  "id" => "245321145541707",
+                  "name" => "Cinbersol"
+                },
+                "position" => {
+                  "id" => "128674630510351",
+                  "name" => "Audio Engineer"
+                },
+                "start_date" => "2011-03"
+              },
+              {
+                "employer" => {
+                  "id" => "116860741677367",
+                  "name" => "Freelance Violinist"
+                },
+                "position" => {
+                  "id" => "145617765464974",
+                  "name" => "Violinist"
+                },
+                "start_date" => "2009-04"
+              },
+              {
+                "employer" => {
+                  "id" => "111078042263414",
+                  "name" => "Musician"
+                },
+                "projects" => [
+                  {
+                    "id" => "109871969042969",
+                    "name" => "Musician for hire",
+                    "start_date" => "2010-09"
+                  }
+                ]
+              }
+            ],
+            "education" => [
+              {
+                "school" => {
+                  "id" => "111845662166573",
+                  "name" => "Westfield High School"
+                },
+                "year" => {
+                  "id" => "141778012509913",
+                  "name" => "2008"
+                },
+                "type" => "High School"
+              },
+              {
+                "school" => {
+                  "id" => "157433874304051",
+                  "name" => "G. Martell"
+                },
+                "concentration" => [
+                  {
+                    "id" => "136073143125360",
+                    "name" => "Music Production & Engineering"
+                  }
+                ],
+                "type" => "College"
+              }
+            ],
+            "gender" => "male",
+            "email" => "knight829@gmail.com",
+            "timezone" => -5,
+            "locale" => "en_US",
+            "verified" => true,
+            "updated_time" => "2012-05-13T03:41:11+0000"
+          }
+        }
+      }
     end
 
     def self.chick
-      {"provider"=>"facebook", "uid"=>"1372100291", "info"=>{"nickname"=>"samantha.diaz.566", "email"=>"shadow01wolf@gmail.com", "name"=>"Samantha Diaz", "first_name"=>"Samantha", "last_name"=>"Diaz", "image"=>"http://graph.facebook.com/1372100291/picture?type=square", "description"=>"I'm a loving person. I get really nervous around some guys, especially if I like them. I try not to show that though. XD;", "urls"=>{"Facebook"=>"http://www.facebook.com/samantha.diaz.566"}, "verified"=>true}, "credentials"=>{"token"=>"AAADMjLRob70BAMSyZCdnbfDlxoBOER56IIHp2cMQYS5NPziDuIy4EWlQinNc7ZBSAfRjJYxWLqbLIzbZAXOcRZCS7z1f3ddNSWzryUYhKAZDZD", "expires_at"=>1353654498, "expires"=>true}, "extra"=>{"raw_info"=>{"id"=>"1372100291", "name"=>"Samantha Diaz", "first_name"=>"Samantha", "last_name"=>"Diaz", "link"=>"http://www.facebook.com/samantha.diaz.566", "username"=>"samantha.diaz.566", "about"=>"I'm Sam. I like to draw, and I like to play the flute. =w= I love a treasured few, and have no enemies as of yet...", "bio"=>"I'm a loving person. I get really nervous around some guys, especially if I like them. I try not to show that though. XD;", "quotes"=>"\"We shall prevail.\"\n\"I can and I will.\"\n\"Sometimes things are harder than they seem.\"", "gender"=>"female", "email"=>"shadow01wolf@gmail.com", "timezone"=>-7, "locale"=>"en_US", "verified"=>true, "updated_time"=>"2012-06-12T12:49:57+0000"}}}
+      {
+        "provider" => "facebook",
+        "uid" => "1372100291",
+        "info" => {
+          "nickname" => "samantha.diaz.566",
+          "email" => "shadow01wolf@gmail.com",
+          "name" => "Samantha Diaz",
+          "first_name" => "Samantha",
+          "last_name" => "Diaz",
+          "image" => "http://graph.facebook.com/1372100291/picture?type=square",
+          "description" => "I'm a loving person. I get really nervous around some guys, especially if I like them. I try not to show that though. XD;",
+          "urls" => {
+            "Facebook" => "http://www.facebook.com/samantha.diaz.566"
+          },
+          "verified" => true
+        },
+        "credentials" => {
+          "token" => "AAADMjLRob70BAMSyZCdnbfDlxoBOER56IIHp2cMQYS5NPziDuIy4EWlQinNc7ZBSAfRjJYxWLqbLIzbZAXOcRZCS7z1f3ddNSWzryUYhKAZDZD",
+          "expires_at" => 1353654498,
+          "expires" => true
+        },
+        "extra" => {
+          "raw_info" => {
+            "id" => "1372100291",
+            "name" => "Samantha Diaz",
+            "first_name" => "Samantha",
+            "last_name" => "Diaz",
+            "link" => "http://www.facebook.com/samantha.diaz.566",
+            "username" => "samantha.diaz.566",
+            "about" => "I'm Sam. I like to draw, and I like to play the flute. =w= I love a treasured few, and have no enemies as of yet...",
+            "bio" => "I'm a loving person. I get really nervous around some guys, especially if I like them. I try not to show that though. XD;",
+            "quotes" => "\"We shall prevail.\"\n\"I can and I will.\"\n\"Sometimes things are harder than they seem.\"",
+            "gender" => "female",
+            "email" => "shadow01wolf@gmail.com",
+            "timezone"=> -7,
+            "locale" => "en_US", "verified" => true,
+            "updated_time" => "2012-06-12T12:49:57+0000"
+          }
+        }
+      }
     end
 
   end
