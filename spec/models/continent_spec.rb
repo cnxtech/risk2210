b1ca990793396e34a3c32a938809e0b82950b933
Nick DeSteffen <nick.desteffen@gmail.com>
2012-12-08 07:45:55 -0600

    Fixing failing spec

--- spec/models/continent_spec.rb
+++ spec/models/continent_spec.rb
@@ -6,7 +6,7 @@ describe Continent do
     it "should order continents by type first and then by name" do
       continents = Continent.ordered.map(&:name)
 
-      continents.should == ["Africa", "Amazonis", "Angeln", "Arcadia", "Asia", "Australia", "Automin", "Avalon", "Balt", "Byfed", "Casius", "Cymru", "Ecclesia", "Eden", "Europe", "Galilei", "Ghaul", "Granada", "Hoenheim", "Narkom", "North America", "Seaxna", "Skandi", "South America", "Tharsis", "Ugricya", "Vulcan", "Caprica", "Khell", "Magreb", "Mashriq", "Nuraghe", "Soter", "1 Ceres", "243 Ida", "4 Vesta", "Cresinion", "Dactyl", "Delphot", "Federation", "New America", "Norseland", "Sajon", "Acidalium", "Arabia", "Asia Pacific", "Indian", "Isidis", "North Atlantic", "Propontis", "South Atlantic", "SungTzu", "US Pacific", "Utopia"]
+      continents.should == ["Africa", "Amazonis", "Angeln", "Arcadia", "Asia", "Australia", "Automin", "Avalon", "Balt", "Byfed", "Casius", "Cymru", "Ecclesia", "Eden", "Europe", "Galilei", "Ghaul", "Granada", "Hoenheim", "Narkom", "North America", "Seaxna", "Skandi", "South America", "Tharsis", "Ugricya", "Vulcan", "Caprica", "Khell", "Magreb", "Mashriq", "Nuraghe", "Soter", "1 Ceres", "243 Ida", "4 Vesta", "Cresinion", "Dactyl", "Delphot", "Federation", "New America", "Norseland", "Sajon", "Acidalium", "Arabia", "Asia Pacific", "Caprica", "Indian", "Isidis", "Khell", "Magreb", "Mashriq", "North Atlantic", "Nuraghe", "Propontis", "Soter", "South Atlantic", "SungTzu", "US Pacific", "Utopia"]
     end
   end
 
