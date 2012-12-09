50b615b69308238772897d9b87ee29019d971c58
Nick DeSteffen <nick.desteffen@gmail.com>
2012-12-09 08:10:37 -0600

    minor refactor

--- spec/lib/states_spec.rb
+++ spec/lib/states_spec.rb
@@ -6,6 +6,9 @@ describe States do
     it "should return the abbreviated name for the state passed in" do
       States.decode(" Wisconsin ").should == "WI"
     end
+    it "should return nil if a state isn't found" do
+      States.decode(" Foo ").should == nil
+    end
   end
 
 end
