cb9ebda395a72a5fde4cd17fdba9e381469f5e78
Nick DeSteffen <nick.desteffen@gmail.com>
2012-12-24 12:01:24 -0600

    Using index file for tracker

---
+++ app/assets/javascripts/risk_tracker/index.js
@@ -0,0 +1,11 @@
+//= require jquery-ui
+//= require underscore-min
+//= require backbone-min
+//= require jquery.animate-shadow-min
+
+//= require ./application
+//= require ./util
+//= require_tree ./templates
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
