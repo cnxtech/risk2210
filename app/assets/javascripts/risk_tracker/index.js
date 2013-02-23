8cf134ed285e1177e162daccd6dd2aae035a043c
Nick DeSteffen <nick.desteffen@gmail.com>
2013-02-23 12:05:22 -0800

    Adding fastclick

--- app/assets/javascripts/risk_tracker/index.js
+++ app/assets/javascripts/risk_tracker/index.js
@@ -1,6 +1,7 @@
 //= require underscore-min
 //= require backbone-min
 //= require jquery.animate-shadow-min
+//= require fastclick
 
 //= require ./application
 //= require ./util
@@ -8,3 +9,7 @@
 //= require_tree ./models
 //= require_tree ./collections
 //= require_tree ./views
+
+window.addEventListener('load', function() {
+  new FastClick(document.body);
+}, false);
