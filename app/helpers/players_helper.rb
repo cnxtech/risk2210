45a2ea6eca350a60f44424de3b888d5ea0668c31
Nick DeSteffen <nick.desteffen@gmail.com>
2013-05-19 17:46:17 -0500

    Bug fix for player avatar helper

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -4,7 +4,7 @@ module PlayersHelper
     States::STATES
   end
 
-  def avatar(player, size=:large)
+  def avatar(player, size: :large)
     image_path = player.profile_image_path(size)
 
     pixels = size == :large ? 150 : 50
