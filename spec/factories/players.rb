e3a67c2d28ed5b204a3f3d54933c08f990c41c5b
Nick DeSteffen <nick.desteffen@gmail.com>
2013-08-13 22:31:28 -0500

    Tweaking player factory

--- spec/factories/players.rb
+++ spec/factories/players.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :base_player, class: "Player" do
-    handle { Faker::Name.first_name }
+    handle { "#{Faker::Name.first_name}-#{rand(100)}" }
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     email { Faker::Internet.free_email }
