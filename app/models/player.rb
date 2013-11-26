603806723d19245ef3700beb248ae98fabd7937c
Nick DeSteffen <nick.desteffen@gmail.com>
2013-11-25 22:29:05 -0600

    Some https fixes

--- app/models/player.rb
+++ app/models/player.rb
@@ -69,16 +69,16 @@ class Player
   ## Facebook image size options
   ## square=50x50, small=50xVariable, normal=100xVariable, large=200xVariable
   def profile_image_path(size=:normal)
-    default_avatar_path = "http://risk2210.net/assets/default_avatar.png"
+    default_avatar_path = "https://risk2210.net/assets/default_avatar.png"
     if image_source == ImageSource::Facebook && facebook_image_url
-      return facebook_image_url + "?type=#{size}"
+      return facebook_image_url.gsub("http://", "https://") + "?type=#{size}"
     elsif image_source == ImageSource::Gravatar
       gravatar_size = case size
         when :square, :small ; 50
         when :normal ; 100
         when :large ; 200
       end
-      return "http://www.gravatar.com/avatar/#{gravatar_hash}?size=#{gravatar_size}&default=#{CGI::escape(default_avatar_path)}"
+      return "https://www.gravatar.com/avatar/#{gravatar_hash}?size=#{gravatar_size}&default=#{CGI::escape(default_avatar_path)}"
     else
       return default_avatar_path
     end
--- spec/helpers/players_helper_spec.rb
+++ spec/helpers/players_helper_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe PlayersHelper do
-  
+
   describe "us_states_options" do
     it "should return an array of us states options" do
       helper.us_states_options.is_a?(Array).should == true
@@ -13,12 +13,12 @@ describe PlayersHelper do
     it "should return the player's avatar for the size requested" do
       avatar_image = helper.avatar(player)
 
-      avatar_image.should == '<img alt="Payton Dog" src="http://risk2210.net/assets/default_avatar.png" width="150px;" />'
+      avatar_image.should == '<img alt="Payton Dog" src="https://risk2210.net/assets/default_avatar.png" width="150px;" />'
     end
     it "should allow me to use a smaller image" do
       avatar_image = helper.avatar(player, size: :small)
 
-      avatar_image.should == '<img alt="Payton Dog" src="http://risk2210.net/assets/default_avatar.png" width="50px;" />'
+      avatar_image.should == '<img alt="Payton Dog" src="https://risk2210.net/assets/default_avatar.png" width="50px;" />'
     end
   end
 
